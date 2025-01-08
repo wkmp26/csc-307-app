e441b088068c9352f803c233debea5382f30f80a
wkmp26 <wkmp26@gmail.com>
2025-01-08 10:14:56 -0800

    Commit after following steps 1-5 of IE1

---
+++ node_modules/react-frontend/src/MyApp.jsx
@@ -0,0 +1,28 @@
+// src/MyApp.jsx
+import React, { useState } from "react";
+import Table from "./Table";
+
+function MyApp() {
+    const [characters, setCharacters] = useState([
+        {
+          name: "Charlie",
+          job: "Janitor"
+        },
+      ]);
+    
+      function removeOneCharacter(index){
+        const updated = characters.filter((character, i) => {
+            return i !== index;
+        });
+        setCharacters(updated);
+      }
+  return (
+    <div className="container">
+      <Table 
+        characterData={characters}
+        removeCharacter={removeOneCharacter}
+        />
+    </div>
+  );
+}
+export default MyApp;
