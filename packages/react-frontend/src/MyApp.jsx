2ca2fbb8d0af9e2610c972da16d49a8b21dc1c48
wkmp26 <wkmp26@gmail.com>
2025-01-08 11:00:26 -0800

    Commit after completion of all IE1 steps

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -6,19 +6,22 @@ import Form from "./Form";
 function MyApp() {
     const [characters, setCharacters] = useState([]);
     
-      function removeOneCharacter(index){
+    function removeOneCharacter(index){
         const updated = characters.filter((character, i) => {
             return i !== index;
         });
         setCharacters(updated);
-      }
+        }
+    function updateList(person) {
+        setCharacters([...characters, person]);
+        }
   return (
     <div className="container">
       <Table 
         characterData={characters}
         removeCharacter={removeOneCharacter}
         />
-        <Form />
+        <Form handleSubmit={updateList} />
     </div>
   );
 }
