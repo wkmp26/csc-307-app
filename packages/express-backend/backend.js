466f633a8b4bcfd230330c8b8a98a1f5120eb09c
Weston <wpatrick@calpoly.edu>
2025-01-20 10:59:47 -0800

    Completion of IE2 steps 3 & 4

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -6,9 +6,22 @@ const port = 8000;
 
 app.use(express.json());
 
-app.get("/users", (req, res) => {
-  res.send(users);
-});
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name
+    );
+  };
+  
+  app.get("/users", (req, res) => {
+    const name = req.query.name;
+    if (name != undefined) {
+      let result = findUserByName(name);
+      result = { users_list: result };
+      res.send(result);
+    } else {
+      res.send(users);
+    }
+  });
 
 app.listen(port, () => {
   console.log(
